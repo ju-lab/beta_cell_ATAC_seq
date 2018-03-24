1878249de9fff6f8f2fb396da6fc45f2b04e5aad
Kijong Yi <kijong.yi@kaist.ac.kr>
2018-03-24 16:56:11 +0900

    ""

--- 02c.bowtie.sh
+++ 02c.bowtie.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-for i in `find -L fastq/ATAC-seq | grep "1.fastq.gz$" | grep trim_`; do
+for i in `find -L fastq/ATAC-seq | grep "1.fastq.gz$" | grep trim_ `; do
 	sample=`echo $i | sed 's!.*/\([^/]*\)_1.fastq.gz$!\1!;s!trim_!!'`
 	dir=`echo $i | sed 's!fastq!bowtie2!;s!\(.*\)/[^/]*!\1!;s!trim_!!'`
 	mkdir -p $dir
 	~/src/atac/run_atac_pipe.sh \
 		--output_bam $dir \
-		--process postalign \
-		--thread 12 \
-		--memory 80GB \
 		$sample \
 		$i \
 		${i/1.fastq.gz/2.fastq.gz}
